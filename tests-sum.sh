651c512c4b54c33548e0ca4ffe66b4f838efec83
Hubert Hesse <hubert.hesse@hubertscorner.de>
2011-08-08 21:14:14 +0200

    parse ERRORS

--- tests-sum.sh
+++ tests-sum.sh
@@ -2,8 +2,10 @@
 # This script parses the amount of failed tests and total tests run
 TOTAL=$(grep "testsuite" $1/*-Test.xml | sed -e "s/^.*tests=\"//" | cut -d "\"" -f1 | grep -v "</testsuite>" )
 FAILURES=$(grep "testsuite" $1/*-Test.xml | sed -e "s/^.*failures=\"//" | cut -d "\"" -f1 | grep -v "</testsuite>")
+ERRORS=$(grep "testsuite" $1/*-Test.xml | sed -e "s/^.*errors=\"//" | cut -d "\"" -f1 | grep -v "</testsuite>")
 
 TOTALSUM=$(echo $TOTAL 0 | tr " " "+" | bc)
 FAILURESSUM=$(echo $FAILURES 0 | tr " " "+" | bc)
+ERRORSSUM=$(echo $ERRORS 0 | tr " " "+" | bc)
 
-echo "SqueakSource-Tests:$FAILURESSUM/$TOTALSUM"
+echo "SqueakSource-Tests:$FAILURESSUM/$ERRORSSUM|$TOTALSUM"
